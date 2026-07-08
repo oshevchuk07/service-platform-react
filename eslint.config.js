b5412410f233480784a4fedeb51bb716cd181b99
oshevchuk07 <oshevchuk07@gmail.com>
2026-07-08 08:45:17 +0300

    chore: fix shadcn/ui component paths, disable react-refresh rule for shared/ui folder

--- eslint.config.js
+++ eslint.config.js
@@ -23,4 +23,10 @@ export default defineConfig([
       },
     },
   },
+  {
+    files: ['src/shared/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  }
 ])
